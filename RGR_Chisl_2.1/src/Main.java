85d3a1c48b43a2ead0b7cda096897f9b20d3ade3
ogeha <xent978@gmail.com>
2024-11-12 16:43:49 +0300

    12345

--- RGR_Chisl_2.1/src/Main.java
+++ RGR_Chisl_2.1/src/Main.java
@@ -7,15 +7,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         x.add(scanner.nextDouble());
         x.add(scanner.nextDouble());
-        ArrayList<Double> y =  new ArrayList<Double>();
+        ArrayList<Double> y_h =  new ArrayList<Double>();
+        ArrayList<Double> y_s = new ArrayList();
+        double h = 1;
+        double s = h/2;
         for (int i = 0; i < 2; i++){
-            y.add(1 / (x.get(i) + 4));
+            y_h.add(1 / (x.get(i) + 4));
         }
-        int h = 1;
-        int s = h/2;
-        double Integri = h * ((y.get(0) + y.get(1)) / 2);
+        for (int i = 0; i < 2; i++){
+            y_s.add(1 / (x.get(i) + 4));
+            y_s.add(1 / ((x.get(i)+s) + 4));
+        }
+        System.out.println(y_h);
+        System.out.println(y_s);
+        double Integri = h * ((y_h.get(0) + y_h.get(1)) / 2);
         System.out.println(Integri);
-        double Integri2 = s * ((y.get(0) + y.get(1)) / 2);
+        double Integri2 = s * ((y_s.get(0) + y_s.get(2)) / 2 + y_s.get(1));
         System.out.println(Integri2);
         double Runge = (Integri - Integri2) / 3;
         System.out.println(Runge);
